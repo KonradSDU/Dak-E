15cc852d7129fd71527978761eef6a5de0b9809a
Konrad Gnoinski <cir7777777@gmail.com>
2014-10-21 20:45:08 +0200

    Initial Commit

---
+++ DAK-E/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DAK-E
+//
+//  Created by Konrad Gnoinski on 21/10/14.
+//  Copyright (c) 2014 Konrad Gnoinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DAK-E/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DAK-E
+//
+//  Created by Konrad Gnoinski on 21/10/14.
+//  Copyright (c) 2014 Konrad Gnoinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
