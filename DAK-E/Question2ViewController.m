1fd96989528cb411b08c10b0620024b65944f139
Konrad Gnoinski <cir7777777@gmail.com>
2014-10-23 00:28:16 +0200

    fix

--- DAK-E/Question2ViewController.m
+++ DAK-E/Question2ViewController.m
@@ -7,33 +7,63 @@
 //
 
 #import "Question2ViewController.h"
+#import "UIColor+customColors.h"
 
 @interface Question2ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *questeionLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *kgLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cmLabel;
+@property (weak, nonatomic) IBOutlet UISlider *kgSlider;
+@property (weak, nonatomic) IBOutlet UISlider *cmSlider;
 @end
 
 @implementation Question2ViewController
 
 - (void)viewDidLoad {
     self.questionNumber = 2;
+    
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [[UIColor darkerGreen] CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImage *minImage = [image resizableImageWithCapInsets:UIEdgeInsetsMake(0, 5, 0, 0)];
+    [[UISlider appearance] setMinimumTrackImage:minImage forState:UIControlStateNormal];
+    [[UISlider appearance] setMaximumTrackImage:minImage forState:UIControlStateNormal];
+    
     self.segueToNextControllerName = @"segueToQuestion3";
+    _questeionLabel1.textColor = [UIColor darkerGreen];
+    _questionLabel2.textColor = [UIColor darkerGreen];
+    _kgLabel.textColor = [UIColor darkerGreen];
+    _kgLabel.backgroundColor = [UIColor whiteColor];
+    _cmLabel.textColor = [UIColor darkerGreen];
+    _cmLabel.backgroundColor = [UIColor whiteColor];
+    _kgSlider.thumbTintColor = [UIColor redColor];
+    _kgLabel.text = [NSString stringWithFormat:@"%d kg", (int)_kgSlider.value];
+    _cmLabel.text = [NSString stringWithFormat:@"%d cm", (int)_cmSlider.value];
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+-(IBAction)sliderValueChanged:(id)sender
+{
+    if (sender == _kgSlider) {
+        _kgLabel.text = [NSString stringWithFormat:@"%d kg", (int)_kgSlider.value];
+    }else{
+        _cmLabel.text = [NSString stringWithFormat:@"%d cm", (int)_cmSlider.value];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
