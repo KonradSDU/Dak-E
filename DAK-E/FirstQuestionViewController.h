d40256a017e7c15c29070f8aa5b2dd02906b58a7
Konrad Gnoinski <cir7777777@gmail.com>
2014-10-22 22:15:21 +0200

    no message

---
+++ DAK-E/FirstQuestionViewController.h
@@ -0,0 +1,14 @@
+//
+//  FirstQuestionViewController.h
+//  DAK-E
+//
+//  Created by Milena Gnoińska on 22.10.2014.
+//  Copyright (c) 2014 Konrad Gnoinski. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "UIColor+customColors.h"
+
+@interface FirstQuestionViewController : BaseViewController
+
+@end
