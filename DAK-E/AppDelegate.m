078ce01c0020404f4fc0b639384c3447c0785773
Konrad Gnoinski <cir7777777@gmail.com>
2014-11-07 17:30:07 +0100

    All questions are now added to questionnaire

--- DAK-E/AppDelegate.m
+++ DAK-E/AppDelegate.m
@@ -29,10 +29,41 @@
         [DatabaseManager insertQuestionaireWithName:@"general"];
         Questionaire *que = [DatabaseManager questionaireWithName:@"general"];
         [DatabaseManager insertQuestionWithQuestion:@"Hvordan synes du dit helbred er alt i alt?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Fremragende", @"Vældigt godt", @"Godt", @"Mindre godt", @"Dårligt", nil] questionaire:que];
-        [DatabaseManager insertQuestionWithQuestion:@"Vægt og højde" questionType:[NSNumber numberWithInt:2] subTitle:nil answers:[NSArray arrayWithObjects:@"Hvad er din vægt?", @"Hvor høj er du?", nil] questionaire:que];
-        [DatabaseManager insertQuestionWithQuestion:@"Ryger du?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Ryger til dagligt", @"Lejlighedsvis ryger", @"Er stoppet for mindre end 6 måneder siden",@"Er stoppet for mere end 6 måneder siden", @"Har aldrig røget (hvis du aldrig har røget, gå da til spørgsmål 7)", nil] questionaire:que];
-        [DatabaseManager insertQuestionWithQuestion:@"Indenfor de sidste 4 uger, har du da haft åndenød i forbindelse med hverdagsaktiviteter? (for eksempel en gåtur, let havearbejde, rengøring, indkøb og lignende) " questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"På intet tidspunkt", @"Sjældent", @"Af og til", @"Ofte", @"Hele tiden", nil] questionaire:que];
         
+        [DatabaseManager insertQuestionWithQuestion:@"Vægt og højde" questionType:[NSNumber numberWithInt:2] subTitle:nil answers:[NSArray arrayWithObjects:@"Hvad er din vægt?", @"Hvor høj er du?", nil] questionaire:que];
+        
+        [DatabaseManager insertQuestionWithQuestion:@"Ryger du?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Ryger til dagligt", @"Lejlighedsvis ryger", @"Er stoppet for mindre end 6 måneder siden",@"Er stoppet for mere end 6 måneder siden", @"Har aldrig røget (hvis du aldrig har røget, gå da til spørgsmål 7)", nil] questionaire:que];
+        
+        [DatabaseManager insertQuestionWithQuestion:@"Indenfor de sidste 4 uger, har du da haft åndenød i forbindelse med hverdagsaktiviteter? (for eksempel en gåtur, let havearbejde, rengøring, indkøb og lignende)" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"På intet tidspunkt", @"Sjældent", @"Af og til", @"Ofte", @"Hele tiden", nil] questionaire:que];
+        
+        [DatabaseManager insertQuestionWithQuestion:@"Hoster du slim op?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Nej, aldrig", @"Kun når jeg er forkølet eller har hals- eller lungebetændelse", @"Ja, et par dage om måneden", @"Ja, de fleste dage på en uge", @"Ja, alle dage", nil] questionaire:que];
+        
+        [DatabaseManager insertQuestionWithQuestion:@"I hvor høj grad er du enig i følgende udsagn. På grund af åndenød har jeg i de sidste 12 måneder bevæget mig mindre end jeg plejer." questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Meget uenig", @"Uenig", @"Måske", @"Enig", @"Meget enig", nil] questionaire:que];
+        
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor mange genstande drikker du i gennemsnit om ugen på en almindelig uge?" questionType:[NSNumber numberWithInt:6] subTitle:@"1 almindelig øl (4,6 %) = 1 genstand \n1 almindelig øl i plastflaske = 1 1/4 genstand \n1 guldøl (5,8 %) = 1 1/4 genstand \n 1 stærk øl (7 %) = 1 1/2 - 2 genstande \n1 almindeligt glas vin (ca. 12 %) = 1 genstand \n1 glas hedvin (8 cl) = 1 genstand \n4 cl. spiritus, 2 små snapse (38 %) = 1 genstand \n1 flaske spiritus (70 cl) = 20 genstande \n1 flaske hedvin (70 cl) = 10 genstande \n1 flaske rødvin (70 cl) = 6 genstande" answers:[NSArray arrayWithObjects:@"Jeg drikker ikke", @"1-6 genstande", @"7-14 genstande", @"15-21 genstande", @"22-28 genstande", @"29- genstande", nil] questionaire:que];
+        //8
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor ofte drikker du mere end 5 genstande ved samme lejlighed?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Dagligt eller næsten dagligt", @"Ugentligt", @"Månedligt", @"Sjældent", @"Aldrig", nil] questionaire:que];
+        //9
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor hyppigt spiser du grøntsager og/eller rodfrugter (friske såvel som frosne)" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"To gange dagligt eller oftere", @"En gang dagligt", @"Nogle gange i løbet af ugen", @"En gang om ugen eller sjældneret", nil] questionaire:que];
+        //10
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor hyppigt spiser du frugt og/eller bær (friske, frosne, konserves eller juice/smoothie)" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"To gange dagligt eller oftere", @"En gang dagligt", @"Nogle gange i løbet af ugen", @"En gang om ugen eller sjældnere", nil] questionaire:que];
+        //11
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor hyppigt spiser du fisk eller skaldyr som hovedret" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Tre gange om ugen eller oftere", @"To gange om ugen", @"En gang om ugen", @"Et par gange om måneden eller sjældnere", nil] questionaire:que];
+        //12
+        [DatabaseManager insertQuestionWithQuestion:@"Hvor hyppigt spiser du wienerbrød, slik, chips og/eller drikker sodavand" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"To gange dagligt eller oftere", @"En gang dagligt", @"Nogle gange i løbet af ugen", @"En gang om ugen eller sjældnere", nil] questionaire:que];
+        //13
+        [DatabaseManager insertQuestionWithQuestion:@"Hvis du ser på det seneste år, hvad ville du så sige passer bedst som beskrivelse af din fysiske aktivitet i fritiden?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Træner hårdt og dyrker konkurrenceidræt regelmæssigt og flere gange om ugen", @"Dyrker motionsidræt eller udfører tungt havearbejde eller lignende mindst 4 timer om ugen", @"Spadserer, cykler eller har anden lettere motion mindst 4 timer om ugen ( medregn også søndagsture, lettere havearbejde og cykling/gang til arbejde", @"Læser, ser fjernsyn eller har anden stillesiddende beskæftigelse", nil] questionaire:que];
+        //14
+        [DatabaseManager insertQuestionWithQuestion:@"Hvis du tænker på dine hofter og knæ, har du så..." questionType:[NSNumber numberWithInt:5] subTitle:nil answers:[NSArray arrayWithObjects:@"Haft vedvarende smerter eller gener i dine knæ eller hofter indenfor den sidste måned?", @"Haft en skade i knæ eller hofter som gjorde at du opsøgte læge?", @"Fået foretaget en operation i knæ eller hofter?", nil] questionaire:que];
+        //15
+        [DatabaseManager insertQuestionWithQuestion:@"Ønsker du hjælp til at ændre din livsstil?" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Ja", @"ja, men ikke lige nu", nil] questionaire:que];
+        //16
+        [DatabaseManager insertQuestionWithQuestion:@"Er der nogen i din nærmeste familie som - inden de blev 70 år - har eller har haft (nærmeste familie er bedsteforældre, forældre, søskende, børn)" questionType:[NSNumber numberWithInt:4] subTitle:nil answers:[NSArray arrayWithObjects:@"Forhøjet blodtryk", @"Forhøjet kolesterol", @"Blodprop i hjertet", @"Blodprop i hjernen", @"Blodprop i lunger eller ben", @"Type-1 diabetes", @"Type-2 diabetes", @"KOL", @"Slidgigt/ artrose", nil] questionaire:que];
+        //17
+        [DatabaseManager insertQuestionWithQuestion:@"Ønsker du at give din e-mail adresse(Din e-mail adresse vil blive brugt til at kommunikere elektronisk mellem dig og sundhedsvæsenet)" questionType:[NSNumber numberWithInt:3] subTitle:nil answers:[NSArray arrayWithObjects:@"E-mail adresse:", nil] questionaire:que];
+        //18
+        [DatabaseManager insertQuestionWithQuestion:@"Må vi kontakte dig igen i forbindelse med yderligere forskning?(Du har fået spørgeskemaet som led i et stort forskningsprojekt. I projektet Undersøges flere spørgsmål relateret til projektets overordnede formål om at fremme sunde levevaner og opspore og forebygge sygdomme forårsaget af en usund livsstil. Det vil her være til stor hjælp, at så mange borgere som muligt vil bidrage til at skabe ny viden på dette område)" questionType:[NSNumber numberWithInt:1] subTitle:nil answers:[NSArray arrayWithObjects:@"Ja, I må gerne kontakte mig i forbindelse med forskning", @"Nej tak, jeg vil IKKE kontaktes yderligere i forbindelse med forskning", nil] questionaire:que];
+
     }
     return YES;
 }
