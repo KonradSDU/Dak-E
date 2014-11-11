b46a0099ec75e127cda5fee9c4ff555a175cd0e6
Konrad Gnoinski <cir7777777@gmail.com>
2014-11-11 17:54:25 +0100

    Pliki mileny

---
+++ AdminModeViewController.m
@@ -0,0 +1,38 @@
+//
+//  AdminModeViewController.m
+//  DAK-E
+//
+//  Created by Milena Gnoińska on 04.11.2014.
+//  Copyright (c) 2014 Konrad Gnoinski. All rights reserved.
+//
+
+#import "AdminModeViewController.h"
+
+@interface AdminModeViewController ()
+
+@end
+
+@implementation AdminModeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.rightNavButt.hidden = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
